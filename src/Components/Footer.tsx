f397dd77f065c8bf091a903815e968b5b5403e36
remew <foolishremew@gmail.com>
2019-02-18 01:33:59 +0900

    Footer use theme

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, {useContext} from 'react';
+
+import ThemeContext from '../Themes/context';
 
 const FOOTER_HEIGHT = 44;
 
 export default function Footer(props: any): any {
+    const {theme} = useContext(ThemeContext);
     return (
       <>
         <footer>© 2015-2019 remew.net</footer>
         <style jsx>{`
         footer {
-          background: #000;
-          color: #fff;
+          background: ${theme.footerBackgroundColor};
+          color: ${theme.footerTextColor};
           height: ${FOOTER_HEIGHT}px;
           line-height: ${FOOTER_HEIGHT}px;
           text-align: center;
