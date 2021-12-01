18463243bbb6a0965d08bf90440e33657796dbc3
remew <foolishremew@gmail.com>
2021-12-01 23:32:08 +0900

    configure additional theme and variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@ module.exports = {
   purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/features/**/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      borderWidth: {
+        16: '16px',
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      textColor: ['visited'],
+      display: ['group-hover'],
+    },
   },
   plugins: [],
 };
