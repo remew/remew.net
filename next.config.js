5ecda89f8f57227b5e871405ce4f088e480934a3
remew <foolishremew@gmail.com>
2021-12-02 00:25:22 +0900

    configure image domains

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ const withTm = require('next-transpile-modules')(['react-syntax-highlighter']);
 
 module.exports = withTm({
   reactStrictMode: true,
+  images: {
+    domains: ['www.google.com', 'storage.googleapis.com', process.env.IMAGE_CDN_DOMAIN],
+  },
 });
