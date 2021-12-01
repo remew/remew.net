c992d332cd1b3fa5338be932f137484ce44dda52
remew <foolishremew@gmail.com>
2021-12-01 23:31:42 +0900

    configure purge for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [],
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/features/**/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -8,4 +8,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
